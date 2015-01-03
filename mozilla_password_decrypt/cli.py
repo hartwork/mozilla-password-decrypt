c6f1e1fd3b64c09f70130533176fc68a16d1bb04
Sebastian Pipping <sebastian@pipping.org>
2015-01-03 01:17:18 +0100

    Port pwdecrypt.c to python ctypes + add simple CLI

---
+++ mozilla_password_decrypt/cli.py
@@ -0,0 +1,100 @@
+# Copyright (C) 2015 Sebastian Pipping <sebastian@pipping.org>
+#
+# Licensed under MPL 1.1 / GPL 2.0 or later / LGPL 2.1 or later
+
+from __future__ import print_function
+
+import json
+import os
+import sqlite3
+import sys
+from argparse import ArgumentParser
+from glob import glob
+
+from .decrypt import decrypt_single, NssInitializationFailedException, Base64DecodingFailedException, PasswordDecryptionFailedException
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('profile_paths', nargs='*', metavar='PATH', help='Profiles to analyze (default: auto detection)')
+    options = parser.parse_args()
+
+    if options.profile_paths:
+        profiles_to_scan = options.profile_paths
+    else:
+        profiles_to_scan = []
+        for pattern in (
+                os.path.expanduser('~/.mozilla/firefox/*.default/'),
+                os.path.expanduser('~/.thunderbird/*.default/'),
+                ):
+            profiles_to_scan += glob(pattern)
+
+    d = {}
+    success = True
+
+    for profile_path in profiles_to_scan:
+        filename = os.path.join(profile_path, 'signons.sqlite')
+        if not os.path.exists(filename):
+            print('Database file "%s" not found' % filename, file=sys.stderr)
+            continue
+
+        try:
+           connection = sqlite3.Connection(filename)
+        except sqlite3.OperationalError as e:
+            print('%s (file "%s")' % (e, filename), file=sys.stderr)
+            continue
+
+        output = []
+        d[profile_path] = output
+
+        cursor = connection.execute('SELECT * FROM moz_logins;')
+        for row in cursor.fetchall():
+            _id, _hostname, _httpRealm, _formSubmitURL, _usernameField, _passwordField, \
+                    _encryptedUsername, _encryptedPassword, \
+                    _guid, _encType, \
+                    _timeCreated, _timeLastUsed, _timePasswordChanged, _timesUsed = \
+                    row
+
+            entry = {
+                    'id': _id,
+                    'hostname': _hostname,
+                    'httpRealm': _httpRealm,
+                    'formSubmitURL': _formSubmitURL,
+                    'usernameField': _usernameField,
+                    'passwordField': _passwordField,
+                    'encryptedUsername': _encryptedUsername,
+                    'encryptedPassword': _encryptedPassword,
+                    'guid': _guid,
+                    'encType': _encType,
+                    'timeCreated': _timeCreated,
+                    'timeLastUsed': _timeLastUsed,
+                    'timePasswordChanged': _timePasswordChanged,
+                    'timesUsed': _timesUsed,
+            }
+            output.append(entry)
+
+            encrypted_encoded = _encryptedPassword.encode('utf-8')
+
+            try:
+                decrypted_password = decrypt_single(profile_path, encrypted_encoded)
+            except NssInitializationFailedException:
+                print('NSS initialization failed for profile path "%s".' % profile_path, file=sys.stderr)
+                sys.exit(1)
+            except Base64DecodingFailedException:
+                print('Base64 decoding failed (database "%s", id %d).' % (filename, _id), file=sys.stderr)
+                success = False
+                continue
+            except PasswordDecryptionFailedException:
+                print('Password decryption failed (database "%s", id %d).' % (filename, _id), file=sys.stderr)
+                success = False
+                continue
+
+            entry['decryptedPassword'] = decrypted_password
+
+        connection.close()
+
+    if d:
+        json.dump(d, sys.stdout, indent=4, sort_keys=True)
+        print()
+
+    sys.exit(int(not success))
---
+++ mozilla_password_decrypt/decrypt.py
@@ -0,0 +1,85 @@
+# Copyright (C) 2009 Nelson Bolyard <nelson%bolyard.com>
+# Copyright (C) 2015 Sebastian Pipping <sebastian%pipping.org>
+#
+# Licensed under MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# This is a port of
+#   security/nss/cmd/pwdecrypt/pwdecrypt.c
+#   licensed under MPL 1.1/GPL 2.0/LGPL 2.1
+#   retrieved from https://github.com/mozilla-services/services-central-legacy/blob/master/security/nss/cmd/pwdecrypt/pwdecrypt.c
+#   on 2015-01-02 21:00 UTC+1
+
+from ctypes import CDLL, Structure, c_char_p, c_int, byref, cast, POINTER, string_at, c_uint as c_enum
+
+
+SECSuccess = 0  # security/nss/lib/util/seccomon.h
+siBuffer = 0  # security/nss/lib/util/seccomon.h
+PR_TRUE = 1  # nsprpub/pr/include/prtypes.h
+PR_FALSE = 0  # nsprpub/pr/include/prtypes.h
+PW_NONE = 0  # modules/libmar/sign/nss_secutil.h
+
+
+class SECItem(Structure):  # security/nss/lib/util/seccomon.h
+    _fields_ = [
+        ('type', c_enum),
+        ('data', c_char_p),
+        ('len', c_int),
+    ]
+
+class secuPWData(Structure):  # modules/libmar/sign/nss_secutil.h
+    _fields_ = [
+        ('source', c_enum),
+        ('data', c_char_p),
+    ]
+
+
+class MozillaPasswordDecryptException(BaseException):
+    pass
+
+class NssInitializationFailedException(MozillaPasswordDecryptException):
+    pass
+
+class Base64DecodingFailedException(MozillaPasswordDecryptException):
+    pass
+
+class PasswordDecryptionFailedException(MozillaPasswordDecryptException):
+    pass
+
+
+def decrypt_single(profile_path, encrypted):
+    nss = CDLL('libnss3.so')
+
+    rv = nss.NSS_Init(profile_path)
+    if rv != SECSuccess:
+        raise NssInitializationFailedException()
+
+    try:
+        decoded_orig = nss.NSSBase64_DecodeBuffer(None, None, encrypted, len(encrypted))
+        if decoded_orig == 0:
+            raise Base64DecodingFailedException()
+
+        decoded = cast(decoded_orig, POINTER(SECItem))
+        if decoded[0].len == 0:
+            raise Base64DecodingFailedException()
+
+        try:
+            result = SECItem()
+            result.type = siBuffer
+            result.data = None
+            result.len = 0
+
+            pwdata = secuPWData()
+            pwdata.source = PW_NONE
+            pwdata.data = None
+
+            rv = nss.PK11SDR_Decrypt(decoded, byref(result), byref(pwdata))
+            if rv != SECSuccess:
+                raise PasswordDecryptionFailedException()
+
+            decrypted = string_at(result.data, result.len)
+            nss.SECITEM_ZfreeItem(byref(result), PR_FALSE);
+            return decrypted
+        finally:
+            nss.SECITEM_ZfreeItem(decoded, PR_TRUE);
+    finally:
+        nss.NSS_Shutdown()
