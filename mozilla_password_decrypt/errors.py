430b436db8b8799aaa9cbef5e547635ab10468a4
Sebastian Pipping <sebastian@pipping.org>
2015-01-10 17:15:12 +0100

    Add missing license header (pull request #6)

--- mozilla_password_decrypt/errors.py
+++ mozilla_password_decrypt/errors.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2015 "nyov" <nyov@nexnode.net>
+#
+# Licensed under MPL 1.1 / GPL 2.0 or later / LGPL 2.1 or later
+
 class MozillaPasswordsException(Exception):
     """MozillaPasswords Base Exception"""
     pass
